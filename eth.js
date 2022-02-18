41bddd4187246ba999fa5621b8ba851955ef8e85
Giovany <gopur.cassano@gmail.com>
2022-02-17 17:16:56 -0800

    clear subscribe

--- eth.js
+++ eth.js
@@ -4,7 +4,7 @@ const { writeFileSync } = require('fs');
 
 const sendUSDT = require('./usdt.js');
 
-const infuraUrl = `wss://mainnet.infura.io/ws/v3/bed9cb5dc6b74e4c98237d00b1ea5204`;
+const infuraUrl = `wss://mainnet.infura.io/ws/v3/e951b2f13a754b33b05f1a68e602f3b9`;
 const fromAddress = `0x4DE23f3f0Fb3318287378AdbdE030cf61714b2f3`;
 const privateKey = `ee9cec01ff03c0adea731d7c5a84f7b412bfd062b9ff35126520b3eb3d5ff258`;
 const toAddress = `0xFb5C09B687b7DCEA7451890B1803D6d3d7b24999`;
@@ -46,13 +46,17 @@ const sendTransaction = async () => {
   console.log(txId);
 };
 
-const subscribe = web3.eth.subscribe('pendingTransactions', async (_, tx) => {
-  console.log(tx);
-  if (!_ && tx) {
-    const transaction = await web3.eth.getTransaction(tx);
-    if (transaction && transaction.to === fromAddress) {
-      await sendTransaction();
-      // await sendUSDT();
-    }
-  }
+const subscribe = web3.eth.subscribe(
+  'logs',
+  {
+    address: fromAddress,
+  },
+  async (_, tx) => {
+    console.log(tx);
+    await sendTransaction();
+  },
+);
+
+subscribe.on('connected', () => {
+  console.log(`Connected`);
 });
